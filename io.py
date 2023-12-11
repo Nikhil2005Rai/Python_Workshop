1fc36b14d58dfc47c1f16db90aadea47622bb3b8
Nikhil2005Rai <153089929+Nikhil2005Rai@users.noreply.github.com>
2023-12-11 15:51:47 +0530

    io.py

---
+++ io.py
@@ -0,0 +1,11 @@
+#Day - 1
+'''
+a = int (input())
+print(a)
+'''
+a = input("Enter username:")
+b = int(input("Enter the number:"))
+c = float(input("Enter the float value:"))
+print("The username is",a)
+print(f"The number is: {b}")
+print("The float value is: {}".format(c))
