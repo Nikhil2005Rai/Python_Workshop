b12bd8e4b47729cddb337a2e04a45187d226e24c
Nikhil2005Rai <153089929+Nikhil2005Rai@users.noreply.github.com>
2023-12-12 10:04:18 +0530

    Update io_1.py

--- io_1.py
+++ io_1.py
@@ -1,5 +1,11 @@
 name = input("Enter your name:")
 branch= input("Enter your branch:")
+
+#Simple Print
 print("My name is",name,"and I am from",branch,"branch.")
+
+#Printusing.format()
 print("My name is {} and Iam from {} branch.".format(name,branch))
+
+#Print using f-string
 print(f"My name is {name} andI am from {branch} branch.")
