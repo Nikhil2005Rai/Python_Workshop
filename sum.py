e9370e9ac4e61818aa3a942f80a768381c52b904
Nikhil2005Rai <153089929+Nikhil2005Rai@users.noreply.github.com>
2023-12-12 10:27:41 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,6 +1,7 @@
 a = float(input("Enter the value of a:"))
 b = float(input("Enter the value of b:"))
+sum =a+b
 print("The sum of",a,"and",b,"is",a+b)
-print("The sum of {0:.4f} and {1:.4f} is {2:.4f}".format(a,b,a+b))
+print("The sum of {2:.4f} and {1:.4f} is {0:.4f}".format(a+b,b,a))
 print(f"The sum of {a} and {b} is {a+b}")
 print("Sum till five places is {0:.4f}".format(a+b))
